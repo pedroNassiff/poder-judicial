2b47610d6ae022121151f85d79e3559bdbc90f50
davicoa <alvarado_z@outlook.com>
2020-08-04 15:56:13 -0300

    as

--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { PageHeader } from 'antd';
+
+import './Header.scss'
 
 const Header = () => {
   return (
-    <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
+    <PageHeader
+      className="site-page-header"
+      onBack={() => null}
+      title="Title"
+      subTitle="This is a subtitle"
+    />
   );
 };
 
--- src/componentes/menuTop/MenuTop.js
+++ src/componentes/menuTop/MenuTop.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 import 'antd/dist/antd.css';
 import { Menu } from 'antd';
 import { SettingOutlined, HomeOutlined, LoginOutlined } from '@ant-design/icons';
+import Header from "componentes/header"
 
 import './MenuTop.scss'
-const { SubMenu, Item, ItemGroup} = Menu;
+const { SubMenu, Item, ItemGroup } = Menu;
 
 export default function MenuTop(props) {
     const [current, serCurrent] = useState('1')
@@ -16,32 +17,35 @@ export default function MenuTop(props) {
     }
 
     return (
-        <Menu style={{ position: "fixed", zIndex: 1, width: "100%" }} onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Item key='1' icon={<HomeOutlined />}  >
-                <Link to={"/"}>
-                    <span className="nav-text">INICIO</span>
-                </Link>
-            </Item>
-            <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
-                <ItemGroup title="Item 1">
-                    <Item key='2'>
-                        <Link to={"/contact"}>
-                            <span className="nav-text">Contact</span>
-                        </Link>
-                    </Item>
-                    <Item key="setting:2">Option 2</Item>
-                </ItemGroup>
-            </SubMenu>
-            <Item key='3' icon={<LoginOutlined />} >
-                <Link to={"/login"}>
-                    <span className="nav-text">Login</span>
-                </Link>
-            </Item>
-            <Item key='4' icon={<LoginOutlined />} >
-                <Link to={"/nueva-cuenta"}>
-                    <span className="nav-text">Nueva Cuenta</span>
-                </Link>
-            </Item>
-        </Menu>
+        <div style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+            <Header style={{ position: "relative"}} />
+            <Menu style={{ position: "relative"}} onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+                <Item key='1' icon={<HomeOutlined />}  >
+                    <Link to={"/"}>
+                        <span className="nav-text">INICIO</span>
+                    </Link>
+                </Item>
+                <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
+                    <ItemGroup title="Item 1">
+                        <Item key='2'>
+                            <Link to={"/contact"}>
+                                <span className="nav-text">Contact</span>
+                            </Link>
+                        </Item>
+                        <Item key="setting:2">Option 2</Item>
+                    </ItemGroup>
+                </SubMenu>
+                <Item key='3' icon={<LoginOutlined />} >
+                    <Link to={"/login"}>
+                        <span className="nav-text">Login</span>
+                    </Link>
+                </Item>
+                <Item key='4' icon={<LoginOutlined />} >
+                    <Link to={"/nueva-cuenta"}>
+                        <span className="nav-text">Nueva Cuenta</span>
+                    </Link>
+                </Item>
+            </Menu>
+        </div>
     )
 }
