fc7bdc46cb4e7e0b0d5a1cc571c7cee10fecddeb
Somos 404 <somos404studio@gmail.com.ar>
2020-08-04 14:12:48 -0300

    Login

--- src/componentes/auth/Login.js
+++ src/componentes/auth/Login.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'componentes/auth/Login.scss'
 const Login = () => {
 
-    const onChange = () => {
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = usuario;
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+
+        //Cuando el usuario quieren logearse
+
+        const onSubimit = e => {
+            e.preventDefault();
+        }
 
     }
     return (
         <div className="form-usuario">
             <div className="contenedor-form">
                 <h1>Iniciar Sesión</h1>
-                    <form>
+                    <form
+                        onSubmit={onSubmit}
+                    >
                         <div className="campo-form">
                             <label htmlFor="email">Email</label>
                             <input
@@ -17,6 +36,7 @@ const Login = () => {
                                 id="email"
                                 name="email"
                                 placeholder="Tu Email"
+                                value={email}
                                 onChange={onChange}
 
                             />
@@ -29,6 +49,7 @@ const Login = () => {
                                 id="password"
                                 name="password"
                                 placeholder="Tu Password"
+                                value={password}
                                 onChange={onChange}
 
                             />
