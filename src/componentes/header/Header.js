0adf018f7db247317c78d35f15473039e23fe8b5
Bran <bbogdanich42@gmail.com>
2020-08-05 01:23:02 -0300

    padding layoutmain fix

--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -1,29 +1,26 @@
 import React from "react";
-import { PageHeader } from 'antd';
-import Logo from 'assets/img/logo4.png'
-import { FacebookOutlined, InstagramOutlined, TwitterOutlined } from '@ant-design/icons';
+import { PageHeader, Avatar } from "antd";
+import Logo from "assets/img/logo4.png";
+import {
+  FacebookOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
 
-import './Header.scss'
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <PageHeader
-      className="site-page-header"
-    >
-      <div className="pageHeaderContainer">
-        <div>
-          <img
-            src={`${Logo}`}
-            alt="content"
-            width="200px"
-            height="auto"
-          />
+    <PageHeader className="site-page-header">
+      <div className="pageContainer">
+        <div className="HeaderContainer">
+          <img src={`${Logo}`} alt="content" width="200px" height="auto" />
         </div>
         <div className="socialRedContainer">
           <div>
-          <FacebookOutlined className="socialRed"/>
-          <InstagramOutlined className="socialRed"/>
-          <TwitterOutlined className="socialRed"/>
+            <Avatar shape="square" className="socialRed" size={45} icon={<FacebookOutlined />} />
+            <Avatar shape="square" className="socialRed" size={45} icon={<InstagramOutlined />} />
+            <Avatar shape="square" className="socialRed" size={45} icon={<TwitterOutlined />} />
           </div>
           <div className="numeroTel">
             <span> +54 0000-0000</span>
