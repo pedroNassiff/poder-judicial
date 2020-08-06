95f2a51fd00462001672c8689d7cddd250088a3a
davicoa <alvarado_z@outlook.com>
2020-08-06 15:27:29 -0300

    next and prev

--- src/componentes/carouselNotices/Carousel.js
+++ src/componentes/carouselNotices/Carousel.js
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
 import "antd/dist/antd.css";
 import "./Carousel.scss";
-import { Carousel } from "antd";
+import { Carousel, Avatar } from "antd";
 import Card from "../card";
 import DummyData from "dummyData/dummyDataCarousel";
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const CarouselNotice = () => {
   const [cards, setCards] = useState();
+
+  const carousel = createRef();
+
+  const handleNext = () => carousel.current.next();
+
+  const handlePrev = () => carousel.current.prev();
 
   useEffect(() => {
     setCards(
@@ -27,17 +34,26 @@ const CarouselNotice = () => {
   }, []);
 
   return (
-    <Carousel
-      autoplay
-      dots={false}
-      infinite={true}
-      slidesToShow={2}
-      slidesToScroll={1}
-      speed={2000}
-      autoplaySpeed={4000}
-    >
-    {cards}  
-    </Carousel>
+    <div className="carouselContainer">
+      <div className="carousel">
+        <Carousel
+          autoplay
+          dots={false}
+          infinite={true}
+          slidesToShow={2}
+          slidesToScroll={1}
+          speed={2000}
+          autoplaySpeed={4000}
+          ref={carousel}
+        >
+          {cards}
+        </Carousel>
+      </div>
+      <div className="iconContainer">
+        <Avatar onClick={handlePrev} shape="circle" className="iconCarousel" size={45} icon={<LeftOutlined />} />
+        <Avatar onClick={handleNext} shape="circle" className="iconCarousel" size={45} icon={<RightOutlined />} />
+      </div>
+    </div>
   );
 };
 
