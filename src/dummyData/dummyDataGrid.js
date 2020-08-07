90065ea2478ee8a3f2f1d7a5445f8bff9ec86687
Bran <bbogdanich42@gmail.com>
2020-08-06 21:01:53 -0300

    card

--- src/dummyData/dummyDataGrid.js
+++ src/dummyData/dummyDataGrid.js
@@ -3,18 +3,18 @@ export default [
         img: 'assets/img/defensoriaPenal.png',
         title: 'Defensorías',
         description: 'Penales Resistencia / Penales Interior',
-        meta: false
+        meta: '4 agosto 2020'
     },
     {
         img: 'assets/img/defensoria.png',
         title: 'Defensorías',
         description: 'Defensorías Públicas Civiles',
-        meta: false
+        meta: '5 agosto 2020'
     },
     {
         img: 'assets/img/asesorias.png',
         title: 'Asesorias ',
         description: 'Asesorias de Ninas, Niños y Adolescentes',
-        meta: false
+        meta: '6 agosto 2020'
     },
 ]
