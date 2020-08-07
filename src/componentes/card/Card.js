570d8a76c12ee6ddf6c6ba36cc1055dc73eee9e0
Bran <bbogdanich42@gmail.com>
2020-08-07 13:07:57 -0300

    card

--- src/componentes/card/Card.js
+++ src/componentes/card/Card.js
@@ -4,29 +4,29 @@ import "./Card.scss";
 const CardComponent = (props) => {
   const { img, title, description, meta } = props;
   return (
-    <div class="box gallery">
-      <div class="card">
-        <div class="background">
+    <div className="box gallery">
+      <div className="card">
+        <div className="background">
           <img width="100%" alt="example" src={require("../../" + img)} />
         </div>
-        <a href="#" class="overlay"></a>
+        <a href="#" className="overlay"></a>
         <a href="#">
-          <i class="fa fa-camera-retro"></i>
+          <i className="fa fa-camera-retro"></i>
         </a>
-        <a class="thumb" href="#">
-          <div class="info">
+        <a className="thumb" href="#">
+          <div className="info">
             <h2>
               <a href="#">{title}</a>
               <a href="#">{description}</a>
             </h2>
-            <div class="foot">
-              <i class="line"></i>
-              <span class="date">{meta}</span>
-              <a href="#" target="_blank" class="social">
-                <i class="fa fa-facebook"></i>
+            <div className="foot">
+              <i className="line"></i>
+              <span className="date">{meta}</span>
+              <a href="#" target="_blank" className="social">
+                <i className="fa fa-facebook"></i>
               </a>
-              <a href="#" target="_blank" class="social">
-                <i class="fa fa-twitter"></i>
+              <a href="#" target="_blank" className="social">
+                <i className="fa fa-twitter"></i>
               </a>
             </div>
           </div>
