307c052bd2658b58c16cd3f3cc5a0b9a4cafd608
Bran <bbogdanich42@gmail.com>
2020-08-06 22:00:57 -0300

    card

--- src/dummyData/dummyDataCarousel.js
+++ src/dummyData/dummyDataCarousel.js
@@ -2,25 +2,25 @@ export default [
     {
         img: 'assets/dummyImg/noticia1.png',
         title: 'Alicia B. Alcalá',
-        description: 'Continuamos  trabajando  en  mesas  intersectoriales  e  interdisciplinarias...',
+        description: 'Continuamos  trabajando  en  mesas  intersectoriales  e...',
         meta: '4 agosto 2020'
     },
     {
         img: 'assets/dummyImg/noticia2.png',
         title: 'Litigio estratégico y lucha por los derechos desde la DEFENSA PÚBLICA',
-        description: 'Las luchas por los derechos de las personas privadas de la libertad no...',
+        description: 'Las luchas por los derechos de las personas privadas de...',
         meta: '4 agosto 2020'
     },
     {
         img: 'assets/dummyImg/noticia3.png',
         title: 'Espacios de Reconocimientos de Derechos',
-        description: 'Siguiendo con la política institucional del Ministerio Pub-lico de la Defensa de...',
+        description: 'Siguiendo con la política institucional del Ministerio Pub-lico...',
         meta: '5 agosto 2020'
     },
     {
         img: 'assets/dummyImg/noticia4.png',
         title: 'Promoción y protección integralde las Personas Adultas Mayores',
-        description: 'En el año 2016, se promulga la ley 7942, hoy N 2553-B, que consagra como su objeto...',
+        description: 'En el año 2016, se promulga la ley 7942, hoy N 2553-B, que consagra...',
         meta: '6 agosto 2020'
     }
 ]
