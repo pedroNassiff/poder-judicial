9bffc2e84418adbd142ccc432cf3c66bcddea9eb
Bran <bbogdanich42@gmail.com>
2020-08-24 18:55:52 -0300

    Whatsapp component

--- src/componentes/whatsapp/WhatsApp.js
+++ src/componentes/whatsapp/WhatsApp.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./WhatsApp.scss";
+import { WhatsAppOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+import WhatsAppConf from '../../config/WhatsApp'
 
 const WhatsApp = () => {
   return (
     <a
-      href="https://web.whatsapp.com/send?phone=543624643147"
+      href={WhatsAppConf.hrefWeb+WhatsAppConf.num}
       className="whatsapp"
       target="_blank"
     >
-      {" "}
-      <i class="fa fa-whatsapp whatsapp-icon"></i>
+      <Avatar
+        shape="circle"
+        style={{ backgroundColor: "#4ced69" }}
+        size={60}
+        icon={<WhatsAppOutlined style={{ color: "white" }} />}
+      />
     </a>
   );
 };
---
+++ src/config/WhatsApp.js
@@ -0,0 +1,6 @@
+export default {
+    num: 543624643147,
+    hrefWeb: "https://web.whatsapp.com/send?phone=",
+    hrefDesk: "https://api.whatsapp.com/send?phone=543624643147",
+
+}
